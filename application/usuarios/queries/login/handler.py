93caab72e41f72ce443003c9ca0636ee56a4ca78
Nayid4 <nayid2004@gmail.com>
2025-10-27 23:28:00 -0500

    Ajuste de caso de uso de iniciar sesion

--- application/usuarios/queries/login/handler.py
+++ application/usuarios/queries/login/handler.py
@@ -1,15 +1,25 @@
 from mediatr import Mediator
-from infrastructure.firebase.firebase_repository import FirebaseUsuarioRepository
+from infrastructure.repositories.usuario_repository import UsuarioRepository
 from werkzeug.security import check_password_hash
 from flask_jwt_extended import create_access_token
-import os
 
 @Mediator.handler
 class LoginUsuarioHandler:
     def handle(self, request):
-        repo = FirebaseUsuarioRepository()
+        repo = UsuarioRepository()
         usuario = repo.obtener_usuario_por_correo(request.correo)
-        if usuario and check_password_hash(usuario.contrasena_hash, request.contrasena):
-            access_token = create_access_token(identity=usuario.correo)
-            return {"access_token": access_token}
-        raise Exception("Credenciales inválidas")
+        if not usuario:
+            raise Exception("Usuario o contraseña incorrectos")
+        if not check_password_hash(usuario.contrasena_hash, request.contrasena):
+            raise Exception("Usuario o contraseña incorrectos")
+        access_token = create_access_token(identity=usuario.id)
+        return {
+            "access_token": access_token,
+            "usuario": {
+                "id": usuario.id,
+                "nombre": usuario.nombre,
+                "apellido": usuario.apellido,
+                "rol": usuario.rol,
+                "correo": usuario.correo
+            }
+        }
