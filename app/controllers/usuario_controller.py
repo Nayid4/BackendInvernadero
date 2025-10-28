7a0b1c8c1ed4d278a5eee1ea7c91f127951bca16
Nayid4 <nayid2004@gmail.com>
2025-10-28 17:13:10 -0500

    Ajuste de endpoints de usuario

--- app/controllers/usuario_controller.py
+++ app/controllers/usuario_controller.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from mediatr import Mediator
-from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, jwt_required
 from flask_problem_details import ProblemDetails, ProblemDetailsError
 
 from application.usuarios.commands.create_usuario.dto import CreateUsuarioDTO
@@ -30,11 +30,12 @@ def login():
         )
 
 @usuario_bp.route('/refresh', methods=['POST'])
-#@jwt_required(refresh=True)
+@jwt_required(refresh=True)
 def refresh():
     usuario_id = get_jwt_identity()
     access_token = create_access_token(identity=usuario_id)
-    return jsonify(access_token=access_token), 200
+    refresh_token = create_refresh_token(identity=usuario_id)
+    return jsonify(access_token=access_token, refresh_token=refresh_token), 200
 
 @usuario_bp.route('/me', methods=['GET'])
 @jwt_required()
